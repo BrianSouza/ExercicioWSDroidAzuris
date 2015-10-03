fdd905e200886958eab246ba9fe86e34c9f0e97c
BrianSouza <brian-desouza@hotmail.com>
2015-10-02 22:19:07 -0300

    webservice

---
+++ ExercicioWSAzuris/Service1.asmx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.Services;
+
+namespace ExercicioWSAzuris
+{
+    /// <summary>
+    /// Summary description for Service1
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    public class Service1 : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+
+        [WebMethod]
+        public int Somar(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+    }
+}
